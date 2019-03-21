6569d4fda1902762e031c6a90cf53e9fcdf351d8
IVB107 <ianvb107@gmail.com>
2019-03-20 19:38:46 -0500

    Completed 'get' & 'post' methods for user route

--- data/users/user-router.js
+++ data/users/user-router.js
@@ -8,13 +8,64 @@ const router = express.Router();
 
 // http request handlers
 
+// GET --> /api/users
 router.get('/', (req, res) => {
-  res.send(`
-    <div>
-      <h1>This is the users page...</h1>
-      <h6>...crickets...</h6>
-    </div>
-  `)
+  db.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500);
+    })
+})
+
+// GET --> /api/users/:id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.getById(id)
+    .then(user => {
+      return !user
+        ? res.status(404).json({ message: 'No user with specified ID' })
+        : res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500);
+    })
+})
+
+// GET --> /api/users/:id/posts
+router.get('/:id/posts', (req, res) => {
+  const { id } = req.params;
+  db.getUserPosts(id)
+    .then(posts => {
+      return !posts
+        ? res.status(404).json({ message: 'Unable to retrieve posts by user with specified ID' })
+        : res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500);
+    })
+})
+
+// POST --> /api/users
+router.post('/', (req, res) => {
+  const newUser = req.body;
+
+  !newUser.name
+    ? res.status(400).json({ message: "Pleas provide a 'name' property & value" })
+    : db.insert(newUser)
+      .then(user => {
+        console.log(user);
+        res.status(200).json(user);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500);
+      })
 })
 
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require('./server.js');
 
-server.listen(5000, () => {
+server.listen(4000, () => {
   console.log('Check out the good ish on Port 5k');
 });
